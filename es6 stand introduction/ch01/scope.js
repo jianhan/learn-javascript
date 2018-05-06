2bed356a06f43601c8e7d5b9b528a9023882434b
Jian Han <jianhan@mail.com>
2018-05-06 23:11:52 +1000

    function declaration

--- es6 stand introduction/ch01/scope.js
+++ es6 stand introduction/ch01/scope.js
@@ -15,9 +15,7 @@ for (var i = 0; i < s.length; i++) {
     console.log(s[i]);
 }
 console.log(i); // 5
-// 上面代码中，变量 i 只用来控制循环，但是循环结束后，它并没有消失，泄露成 了全局变量
-
-// es6 let scope
+// 上面代码中，变量 i 只用来控制循环，但是循环结束后，它并没有消失，泄露成 了全局变量 es6 let scope
 
 function f1() {
     let x = 1
@@ -28,3 +26,34 @@ function f1() {
 }
 
 f1()
+
+// ES6 规定，块级作用域之中，函数声明语句的行为类似于 let ，在块级作用域之外不可引用 ES5 example
+
+function f() {
+    console.log('I am outside!');
+}(function () {
+    if (false) {
+        // 重复声明一次函数f
+        function f() {
+            console.log('I am inside!');
+        }
+    }
+    f();
+}());
+
+// 上面代码在 ES5 中运行，会得到“I am inside!”，因为在 if 内声明的函数 f 会被提升到函数头部，实际运行的代码如下。 上面的代码在符合
+// ES6 的浏览器中，都会报错 考虑到环境导致的行为差异太大，应该避免在块级作用域内声明函数。如果确实需 要，也应该写成函数表达式，而不是函数声明语句
+// 函数声明语句
+{
+    let a = 'secret';
+    function f() {
+        return a;
+    }
+}
+{ // 函数表达式
+    let
+    a = 'secret'; let
+    f = function () {return
+        a;
+    };
+}
