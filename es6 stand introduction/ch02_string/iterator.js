bc8ae99ec623c48c84c36f33768d759bb8440fba
Jian Han <jianhan@mail.com>
2018-05-07 11:24:37 +1000

    Repeat example

--- es6 stand introduction/ch02_string/iterator.js
+++ es6 stand introduction/ch02_string/iterator.js
@@ -20,4 +20,28 @@ console.log(s.includes('o')) // true
 var s = 'Hello world!';
 console.log(s.startsWith('world', 6)) // true
 console.log(s.endsWith('Hello', 5)) // true
-console.log(s.includes('Hello', 6)) 
+console.log(s.includes('Hello', 6))
+
+// repeat 方法返回一个新字符串，表示将原字符串重复 n 次
+'x'.repeat(3) // "xxx"
+'hello'.repeat(2) // "hellohello"
+'na'.repeat(0) // ""
+
+// 参数如果是小数，会被取整
+'na'.repeat(2.9) // "nana"
+
+// 如果 repeat 的参数是负数或者 Infinity ，会报错
+'na'.repeat(Infinity)
+// RangeError
+'na'.repeat(-1)
+// RangeError
+
+// 但是，如果参数是0到-1之间的小数，则等同于0，这是因为会先进行取整运算。0
+// 到-1之间的小数，取整以后等于 -0 ， repeat 视同为0。
+'na'.repeat(-0.9) // ""
+// 参数 NaN 等同于0。
+'na'.repeat(NaN) // ""
+
+// 如果 repeat 的参数是字符串，则会先转换成数字。
+'na'.repeat('na') // ""
+'na'.repeat('3') // "nanana"
