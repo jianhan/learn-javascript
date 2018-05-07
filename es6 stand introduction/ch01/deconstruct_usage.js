b94d1e1e3ec247b7da29547eea6103013b39494c
Jian Han <jianhan@mail.com>
2018-05-07 11:17:56 +1000

    IMport module example

--- es6 stand introduction/ch01/deconstruct_usage.js
+++ es6 stand introduction/ch01/deconstruct_usage.js
@@ -83,3 +83,8 @@ for (let [key] of map) {
 for (let [, value] of map) {
   // ...
 }
+
+// 7. 输入模块的指定方法
+// 加载模块时，往往需要指定输入哪些方法。解构赋值使得输入语句非常清晰
+
+const { SourceMapConsumer, SourceNode } = require("source-map")
