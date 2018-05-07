04920a06c9de55c12db805db54310b44027c9311
Jian Han <jianhan@mail.com>
2018-05-07 11:03:19 +1000

    Default value in param

--- es6 stand introduction/ch01/params.js
+++ es6 stand introduction/ch01/params.js
@@ -4,3 +4,14 @@ function add([x, y]) {
   return x + y;
 }
 console.log(add([1, 2]))
+
+console.log([[1, 2], [3, 4]].map(([a, b]) => a + b))
+
+// 函数参数的解构也可以使用默认值
+function move({x = 0, y = 0} = {}) {
+  return [x, y];
+}
+console.log(move({x: 3, y: 8})); // [3, 8]
+console.log(move({x: 3})); // [3, 0]
+console.log(move({})); // [0, 0]
+console.log(move()); // [0, 0]
